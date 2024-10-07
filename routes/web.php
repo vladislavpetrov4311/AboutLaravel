372e849f592592a047a602499d736446699ce871
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-10-07 12:20:05 +0300

    lesson 5. Model

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class , 'getInfo']);
+Route::get('/post', [PostController::class , 'getData']);
