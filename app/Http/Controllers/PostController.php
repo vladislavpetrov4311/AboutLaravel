20a85f700ab519e630b70586d464aa418afde0fb
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-10-07 12:26:01 +0300

    lesson 5. Model (where+get)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function getData() {
-        $post = Post::where('post' , 'блог 2')->first(); // Получаем пост по фильтрации
+        $post = Post::where('post' , 'блог 2')->get(); // Получаем пост по фильтрации в види коллекции (массива)
 
         dump($post);
     }
