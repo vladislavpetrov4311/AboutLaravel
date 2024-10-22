5f37bdd7997f9e05312cb5de49f18dcdeca0c1a0
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-10-22 21:54:04 +0300

    lesson 16. Class Request for controller

--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -7,15 +7,13 @@ use App\Models\Post;
 use App\Models\User;
 use DB;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 
 class UpdateController extends Controller
 {
-    public function __invoke($id) {
+    public function __invoke($id , UpdateRequest $request) {
         $post = Post::findOrFail($id); // findOrFail - успешный поиск или 404 ошибка
-        $data = request()->validate([
-            'title' => 'string',
-            'post' => 'string'
-        ]);
+        $data = $request->validated();
         $post->update($data);
         return redirect()->route('post.index');
     }
